566b93c8de2108a19f493e5c8fc0edc4fbf65543
Rajat Khandelwal <rajatgupta59@gmail.com>
2016-03-02 18:46:39 +0530

    query status added

---
+++ index.js
@@ -0,0 +1,50 @@
+var request = require("request");
+var method = LensClient.prototype;
+
+function LensClient(parameters) {
+    this.lensServerBaseUrl = parameters.lensServerBaseUrl;
+    this.username = parameters.username;
+    this.password = parameters.password;
+    var lensClient = this;
+
+    this.getSession = function (callback) {
+        if (lensClient._session) {
+            return callback(_session);
+        } else {
+            request.post({
+                url: lensClient.lensServerBaseUrl + "session/",
+                formData: {username: lensClient.username, password: lensClient.password}
+            }, function (error, response, body) {
+                if (response && response.statusCode == 200) {
+                    lensClient._session = body;
+                    callback(lensClient._session);
+                } else {
+                    console.error("Error getting session: " + error);
+                }
+            });
+        }
+    };
+    this.getQueryStatus = function (handle, callback) {
+        this.getSession(function (sessionHandle) {
+            request.get({
+                url: lensClient.lensServerBaseUrl + "/queryapi/queries/" + handle,
+                qs: {sessionid: sessionHandle}, headers: {
+                    'accept': 'application/json'
+                }
+            }, function (error, response, body) {
+                if(response && response.statusCode == 200) {
+                    callback(JSON.parse(body));
+                } else {
+                    console.log("Error getting query status: " + error);
+                }
+            })
+        })
+    };
+    this.executeAsync = function (sql) {
+
+    }
+}
+module.exports = LensClient;
+
+
+
